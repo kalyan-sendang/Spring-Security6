d9b5b5ead3f61916b3b16facd3124926b424faf4
kalyan-sendang <kalyansendang10@gmail.com>
2024-03-20 20:37:11 +0545

    Add csrffiltercookie

--- springsecuritysection5/src/main/java/com/springsecurity/practise/controller/CardsController.java
+++ springsecuritysection5/src/main/java/com/springsecurity/practise/controller/CardsController.java
@@ -1,14 +1,28 @@
 package com.springsecurity.practise.controller;
 
+import com.springsecurity.practise.model.Cards;
+import com.springsecurity.practise.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CardsController {
 
+    @Autowired
+    private CardsRepository cardsRepository;
+
     @GetMapping("/myCards")
-    public String getCardDetails() {
-        return "Here are the card details from the DB";
+    public List<Cards> getCardDetails(@RequestParam int id) {
+        List<Cards> cards = cardsRepository.findByCustomerId(id);
+        if (cards != null ) {
+            return cards;
+        }else {
+            return null;
+        }
     }
 
 }
