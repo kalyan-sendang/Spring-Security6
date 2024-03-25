f97634170eda260cd36adf57962743bc16653533
kalyan-sendang <kalyansendang10@gmail.com>
2024-03-25 17:49:09 +0545

    Learn to add Custom filters

--- springsecuritysection5/src/main/java/com/springsecurity/practise/config/ProjectSecurityConfig.java
+++ springsecuritysection5/src/main/java/com/springsecurity/practise/config/ProjectSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
